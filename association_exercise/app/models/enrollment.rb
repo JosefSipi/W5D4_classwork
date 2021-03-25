65c0b2a0a09340f87a531e03521038bbc2156ded
wzheng10 <wzheng100@gmail.com>
2021-03-25 11:54:55 -0700

    Enrollment

--- association_exercise/app/models/enrollment.rb
+++ association_exercise/app/models/enrollment.rb
@@ -7,8 +7,7 @@ class Enrollment < ApplicationRecord
 
     belongs_to :course,
         primary_key: :id,
-        foreign_key: :
+        foreign_key: :course_id,
+        class_name: :Course
 
-
-    
 end
